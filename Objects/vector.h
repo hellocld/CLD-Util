765d796037b852037d0beddd076f59bba5f81722
Chris Langford <chris@hellocld.com>
2014-07-03 09:48:06 -0400

    Created vector.h
    Contains structs for vectors (currently just a 2D one)

---
+++ Objects/vector.h
@@ -0,0 +1,20 @@
+// 
+// Program:		vector.h
+// Author:		Chris Langford
+// Date:		07/03/2014
+// Description:		A set of structs for storing vector information
+//
+
+#ifndef CLD_UTIL_OBJECTS_VECTOR_H
+#define CLD_UTIL_OBJECTS_VECTOR_H
+
+namespace CLD_Util {
+	namespace Objects {
+		struct vec2d {
+			float x;
+			float y;
+		};
+	}
+}
+
+#endif

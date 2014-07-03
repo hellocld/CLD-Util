c4cd461c957def07f435f907da65c2566062025d
Chris Langford <chris@hellocld.com>
2014-07-03 09:47:39 -0400

    Created Box struct
    Stores position and dimensions of a rectangle

---
+++ Objects/box.h
@@ -0,0 +1,22 @@
+// 
+// Program:		box.h
+// Author:		Chris Langford
+// Date:		07/03/2014
+// Description:		A simple struct for storing rectangle information
+//
+
+#ifndef CLD_UTIL_OBJECTS_BOX_H
+#define CLD_UTIL_OBJECTS_BOX_H
+
+namespace CLD_Util {
+	namespace Objects {
+		struct Box {
+			float x;
+			float y;
+			float w;
+			float h;
+		};
+	}
+}
+
+#endif

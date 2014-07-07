7ede33d9119058bbad8d4321580b779aed9ff613
Chris Langford <chris@hellocld.com>
2014-07-07 10:50:22 -0400

    Added operator overloads to vec2d
    Now you can add, subtract, multiply and divide vec2d objects, as
    well as output them to ostream

--- Objects/vector.h
+++ Objects/vector.h
@@ -11,8 +11,48 @@
 namespace CLD_Util {
 	namespace Objects {
 		struct vec2d {
+			vec2d(float xx, float yy)
+				:x(xx), y(yy){};
 			float x;
 			float y;
+
+			//operator overloads
+			vec2d operator+ (const vec2d& other) {
+				float rx = x + other.x;
+				float ry = y + other.y;
+				return vec2d(rx, ry);
+			}
+			vec2d operator- (const vec2d& other) {
+				float rx = x - other.x;
+				float ry = y- other.y;
+				return vec2d(rx, ry);
+			}
+			vec2d operator* (const vec2d& other) {
+				float rx = x * other.x;
+				float ry = y * other.y;
+				return vec2d(rx, ry);
+			}
+			vec2d operator* (float i) {
+				float rx = x * i;
+				float ry = y * i;
+				return vec2d(rx, ry);
+			}
+			vec2d operator/ (const vec2d& other) {
+				float rx = x / other.x;
+				float ry = y / other.y;
+				return vec2d(rx, ry);
+			}
+			vec2d operator/ (float i) {
+				float rx = x / i;
+				float ry = y / i;
+				return vec2d(rx, ry);
+			}
+
+			friend ostream &operator<< (ostream &out, vec2d v) {
+				out<<"x:\t"<<v.x<<"\n";
+				out<<"y:\t"<<v.y<<"\n";
+			}
+
 		};
 	}
 }
